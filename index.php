588ae1561084ce33d2acd2846957eaad7769c0b0
jensjakob <jensjakob@users.noreply.github.com>
2018-05-28 13:47:59 +0200

    release candidate

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 <body>
 
 <div id="flower">
-	<img id="logo" src="vildblom-logo.png" width="150" height="100" alt="Vildblom">
+	<img id="logo" src="<?php echo get_bloginfo('stylesheet_directory'); ?>/vildblom-logo.png" width="150" height="100" alt="Vildblom">
 	<a id="show-nav" href="#" onClick="document.getElementById('tree').style.display = 'block'; return false;">
 		<img src="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMjUiIGhlaWdodD0iMjQiIHZpZXdCb3g9IjAgMCAyNSAyNCIgZmlsbD0ibm9uZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KPHBhdGggZD0iTTAgMEgyNSIgdHJhbnNmb3JtPSJ0cmFuc2xhdGUoMCAyMSkiIHN0cm9rZT0iYmxhY2siIHN0cm9rZS13aWR0aD0iNSIvPgo8cGF0aCBkPSJNMCAwSDI1IiB0cmFuc2Zvcm09InRyYW5zbGF0ZSgwIDEyKSIgc3Ryb2tlPSJibGFjayIgc3Ryb2tlLXdpZHRoPSI1Ii8+CjxwYXRoIGQ9Ik0wIDBIMjUiIHRyYW5zZm9ybT0idHJhbnNsYXRlKDAgMykiIHN0cm9rZT0iYmxhY2siIHN0cm9rZS13aWR0aD0iNSIvPgo8L3N2Zz4K">
 	</a>
@@ -20,9 +20,7 @@
 </div>
 
 <article>
-<article>
 	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-		<?php the_title(); ?>
 		<?php the_content(); ?>
 	<?php endwhile; endif; ?>
 </article>
@@ -34,7 +32,6 @@
 		'orderby' => 'menu_order',
 		'order' => 'asc',
 		'post_parent' => 0,
-		'posts_per_page' => 3,
 		'offset' => 1
 	);
 	
@@ -42,13 +39,21 @@
 	?>
 	<?php while ($the_query -> have_posts()) : $the_query -> the_post(); ?>
 
-		<div id="glass">
+		<div id="glass" class="id-<?= get_the_ID() ?>">
 			<?php if (has_post_thumbnail()): ?>
-				<a href="<?php the_permalink() ?>"><?php the_post_thumbnail('window-preview'); ?></a>
+				<?php if ( !empty( get_the_content() ) ): ?>
+					<a href="<?php the_permalink() ?>"><?php the_post_thumbnail('window-preview'); ?></a>
+				<?php else: ?>
+					<?php the_post_thumbnail('window-preview'); ?>
+				<?php endif; ?>
 			<?php else: ?>
 				<div class="textbox">
 					<div>
+					<?php if ( !empty( get_the_content() ) ): ?>
 						<a href="<?php the_permalink() ?>"><?php echo get_post_meta(get_the_ID(), 'textruta', true); ?></a>
+					<?php else: ?>
+						<?php echo get_post_meta(get_the_ID(), 'textruta', true); ?>
+					<?php endif; ?>
 					</div>
 				</div>
 			<?php endif; ?>
